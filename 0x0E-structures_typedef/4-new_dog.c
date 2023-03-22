4dfdd7ef00c0adc59224e519ffc8efe08aea0f87
Chinonsocodes <chinonsoesenwanne@gmail.com>
2023-03-21 18:01:11 -0700

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
-
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
-
 /**
   * new_dog - ...
   * @name: ...
@@ -16,41 +14,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *max_dog;
 	int name_l = 0, own_l = 0;
-
 	if (name != NULL && owner != NULL)
+
 	{
 		name_l = _strlen(name) + 1;
 		own_l = _strlen(owner) + 1;
 		max_dog = malloc(sizeof(dog_t));
-
 		if (max_dog == NULL)
 			return (NULL);
-
 		max_dog->name = malloc(sizeof(char) * name_l);
-
 		if (max_dog->name == NULL)
 		{
 			free(max_dog);
 			return (NULL);
 		}
-
 		max_dog->owner = malloc(sizeof(char) * own_l);
-
 		if (max_dog->owner == NULL)
 		{
 			free(max_dog->name);
 			free(max_dog);
 			return (NULL);
 		}
-
 		max_dog->name = _strcpy(max_dog->name, name);
 		max_dog->owner = _strcpy(max_dog->owner, owner);
 		max_dog->age = age;
 	}
-
 	return (max_dog);
 }
-
 /**
   * _strlen - Returns the length of a string
   * @s: String to count
@@ -60,15 +50,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 int _strlen(char *s)
 {
 	int c = 0;
-
 	for (; *s != '\0'; s++)
 	{
 		c++;
 	}
-
 	return (c);
 }
-
 /**
   * _strcpy - Copy a string
   * @dest: Destination value
@@ -79,13 +66,10 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-
 	dest[i++] = '\0';
-
 	return (dest);
 }
